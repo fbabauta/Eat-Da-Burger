0f1c58686aad364b8883f3b0c1bab59af4c934d3
Francine Babauta <francine.babauta@outlook.com>
2021-01-16 03:59:15 -0800

    added models and public folders

---
+++ models/burger.js
@@ -0,0 +1,46 @@
+// require orm object
+const orm = require("../config/orm");
+
+
+// set up burger object using orm methods specific to burger object. sets up methods to interact wth the database
+
+let burger = {
+
+    // select all burgers
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    // insert new burger into burgers table
+    insertOne: function (burgerName, cb) {
+        orm.insertOne("burgers", "burger_name", burgerName, cb, function (res) {
+            cb(res);
+        });
+    },
+    
+    // update devoured column to eaten -> default (false) to true
+    updateDevoured: function (eatenBoolean, burgerID, cb) {
+        orm.updateOne("burgers", "devoured", eatenBoolean, "id", burgerID, cb, function (res) {
+            cb(res);
+        });
+    },
+
+    // update stars rating
+    updateRating: function (ratingNum, burgerID, cb) {
+        orm.updateOne("burgers", "rating", ratingNum, "id", burgerID, cb, functiono(res){
+            cb(res);
+        });
+    },
+
+    // delete burger
+    deleteOne: function (id, cb) {
+        orm.deleteOne("burgers", "id", id, function (res) {
+            cb(res);
+        });
+    };
+};
+
+// export burger object and methods
+module.exports = burger;
