733e16c14b9fe5054f1ea48a99b010484917cb16
francine babauta <francine.babauta@outlook.com>
2021-01-16 07:27:01 -0800

    add styles

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
 var PORT = process.env.PORT || 8000;
+
 var app = express();
 
 // Serve static content for the app from the "public" folder
@@ -10,15 +11,17 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
+// Import routes and give server access
+var routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
 app.listen(PORT, function () {
-    console.log("Listening on port:%s", PORT);
+    console.log("App now listening at localhost:" + PORT);
 });
