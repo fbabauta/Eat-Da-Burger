7721c2e3aa6f93fc17a538bca9ae30377b7ad0c0
francine babauta <francine.babauta@outlook.com>
2021-01-18 17:07:59 -0800

    restructured files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,64 +5,64 @@ const router = express.Router();
 
 // GET ALL BURGERS ROUTE -- html route
 router.get("/", function(req, res) {
-    burger.selectAll(data => {
-        let burgers = { burgerInst: data};
-        res.render("index", burgers);
-    });
+    burger.selectAll(function (data) {
+            let burgers = { burgerInst: data };
+            res.render("index", burgers);
+        });
 });
 
 // API ROUTES //
 
 // GET STARS
 router.get("/api/burgers", function(req, res) {
-    burger.selectAll(data => {
-        res.json(data);
-    });
+    burger.selectAll(function (data) {
+            res.json(data);
+        });
 });
 
 // INSERT ROUTE
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne(req.body.burger_name, result => {
-        res.json({ id: result.insertId });
-    });
+    burger.insertOne(req.body.burger_name, function (result) {
+            res.json({ id: result.insertId });
+        });
 });
 
 // UPDATE DEVOURED
 router.put("/api/burgers/:id", function (req, res) {
     let burgerID = req.params.id;
 
-    burger.updateDevoured(req.body.devoured, burgerID, result => {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+    burger.updateDevoured(req.body.devoured, burgerID, function (result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
 });
 
 // UPDATE STARS RATING
 router.put("/api/burgers/rating/:id", function(req, res) {
     let burgerID = req.params.id;
 
-    burger.updateRating(req.body.rating, burgerID, result => {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+    burger.updateRating(req.body.rating, burgerID, function (result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
 });
 
 // DELETE ROUTE
 router.delete("/api/burgers/:id", function (req, res) {
     let burgerID = req.params.id;
 
-    burger.deleteOne(burgerID, result => {
-        if (result.affectedRows === 0) {
-            return res.status(404).end();
-        }
-        res.status(200).end();
-    });
+    burger.deleteOne(burgerID, function (result) {
+            if (result.affectedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        });
 });
 
 
