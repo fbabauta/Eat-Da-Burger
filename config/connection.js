5caa1587ad003f4ff4ecd4e928d7c2083736a21c
francine babauta <francine.babauta@outlook.com>
2021-02-01 03:32:10 -0800

    no further changes

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "ltd5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: "xoiqel7wsnjdk8uc",
     password: "gq4pm3tqs2owunzu",
