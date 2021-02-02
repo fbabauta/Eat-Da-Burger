d36f7ba5cefe785350c15109f250add3b40e5d34
francine babauta <francine.babauta@outlook.com>
2021-02-01 18:22:01 -0800

    fixed connection and .env files proper file structuring

--- config/connection.js
+++ config/connection.js
@@ -11,10 +11,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 3306,
-    user: "xoiqel7wsnjdk8uc",
-    password: "gq4pm3tqs2owunzu",
+    user: "root",
+    password: process.env.DB_PASSWORD,
     database: "burgers_db"
   });
 }
