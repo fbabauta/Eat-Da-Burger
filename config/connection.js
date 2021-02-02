fb7b11cdc4b0991f97a08b86098747c3dfd7be4d
francine babauta <francine.babauta@outlook.com>
2021-02-01 18:51:58 -0800

    file structuring for .env file

--- config/connection.js
+++ config/connection.js
@@ -11,13 +11,13 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "burgers_db"
+    database: "burgers_db",
   });
-}
+};
 
 // Make mysql connection
 connection.connect(function (err) {
